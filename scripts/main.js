eb01521e8fceebf7b5f30cd70671cba2011ea28c
Myles Proctor <proctorm@cua.edu>
2025-03-23 16:57:32 -0400

    3/23 5pm

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const content = {
+    countries: [
+        {
+            // England
+            flag: "images/England.webp", 
+            link: "https://www.premierleague.com/", 
+            alt: "Premier League"
+        }, 
+        {
+            // Spain
+            flag: "images/Spain.webp", 
+            link: "https://www.laliga.com/en-GB", 
+            alt: "La Liga"
+        }, 
+        {
+            // Italy
+            flag: "images/Italy.webp", 
+            link: "https://www.legaseriea.it/en", 
+            alt: "Serie A"
+        }, 
+        {
+            // Germany
+            flag: "images/Germany.png", 
+            link: "https://www.bundesliga.com/en/bundesliga", 
+            alt: "Bundesliga"
+        },
+        {
+            // France
+            flag: "images/France.png", 
+            link: "https://ligue1.com/", 
+            alt: "Ligue 1"
+        }
+    ]
+}
